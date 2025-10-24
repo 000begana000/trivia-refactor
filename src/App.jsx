865ebd2a12a41af64b061ac5c0b4cbf5f240a646
000begana000 <begana.contact@gmail.com>
2025-10-24 17:42:04 +0200

    feat: create page state and handleChangePage function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import Login from "./components/Login";
 
 export default function App() {
+  const [page, setPage] = useState("login");
+
+  function handleChangePage(pageName) {
+    setPage(pageName);
+  }
+
   return (
     <>
       <Header />
-      <Login />
+      {page === "login" && <Login onChangePage={handleChangePage} />}
     </>
   );
 }
