234b4cbf9289a0bab86fb791b98f2cb60a93785f
000begana000 <begana.contact@gmail.com>
2025-10-25 12:49:10 +0200

    fix: refactor player state

--- src/store/player-context.jsx
+++ src/store/player-context.jsx
@@ -1,20 +1,21 @@
 import { createContext, useState } from "react";
 
 export const PlayerContext = createContext({
-  playerName: "",
-  onSavePlayerName: () => {},
+  player: { playerName, currentScore, highScore },
+  onCreatePlayer: () => {},
 });
 
 export default function PlayerContextProvider({ children }) {
-  const [playerName, setPlayerName] = useState("");
+  const [player, setPlayer] = useState("");
 
-  function handleSavePlayerName(playerName) {
-    setPlayerName(playerName);
+  function handleCreatePlayer(playerName) {
+    const newPlayer = { playerName, currentScore: 0, highScore: 0 };
+    setPlayer(newPlayer);
   }
 
   const ctxValue = {
-    playerName,
-    onSavePlayerName: handleSavePlayerName,
+    player,
+    onCreatePlayer: handleCreatePlayer,
   };
 
   return (
