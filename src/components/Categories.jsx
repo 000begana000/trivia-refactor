51e0be4c86290bfe24c692da311432f89f56c0ee
000begana000 <begana.contact@gmail.com>
2025-10-28 15:47:51 +0100

    feat: display categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,3 +1,30 @@
+import { useState } from "react";
+
+const CATEGORIES = [
+  { id: 9, name: "General Knowledge" },
+  { id: 11, name: "Film" },
+  { id: 12, name: "Music" },
+  { id: 15, name: "Video Games" },
+  { id: 17, name: "Science & Nature" },
+  { id: 22, name: "Geography" },
+  { id: 23, name: "History" },
+  { id: 27, name: "Animals" },
+  { id: 31, name: "Japanese Anime & Manga" },
+];
+
 export default function Categories() {
-  return <h1>Categories</h1>;
+  const [categoryId, setCategoryId] = "";
+  const [categoryName, setCategoryName] = useState("");
+
+  return (
+    <div>
+      <ul>
+        {CATEGORIES.map(category => (
+          <li key={category.id}>
+            <button>{category.name}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
