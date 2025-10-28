2ea2abac3339d64334f46b2e388898556e45208e
000begana000 <begana.contact@gmail.com>
2025-10-28 17:08:59 +0100

    feat: fetch quiz items

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const CATEGORIES = [
   { id: 9, name: "General Knowledge" },
@@ -13,18 +13,55 @@ const CATEGORIES = [
 ];
 
 export default function Categories() {
-  const [categoryId, setCategoryId] = "";
+  const [categoryId, setCategoryId] = useState("");
   const [categoryName, setCategoryName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchQuiz() {
+      if (!categoryId) return;
+
+      try {
+        const response = await fetch(
+          `https://opentdb.com/api.php?amount=10&category=${categoryId}&difficulty=easy&type=boolean`
+        );
+
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("faild to fetch quiz data");
+        }
+
+        console.log(resData.results);
+      } catch (error) {
+        setError({ message: error.message || "Could not fetch quiz items." });
+      }
+    }
+
+    fetchQuiz();
+  }, [categoryId]);
+
+  function handleSelectCategoryName(category) {
+    setCategoryName(category.name);
+    setCategoryId(category.id);
+  }
+
+  function handleStartGame() {}
 
   return (
     <div>
       <ul>
         {CATEGORIES.map(category => (
           <li key={category.id}>
-            <button>{category.name}</button>
+            <button onClick={() => handleSelectCategoryName(category)}>
+              {category.name}
+            </button>
           </li>
         ))}
       </ul>
+      {categoryName && <p>You've selected "{categoryName}" category</p>}
+      <button onClick={handleStartGame}>Start new game</button>
     </div>
   );
 }
