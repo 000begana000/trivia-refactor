ecf326cdc31a4fc57f0f6eadcef2f2a07164e6dc
000begana000 <begana.contact@gmail.com>
2025-10-29 12:29:56 +0100

    feat: display player status

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,13 +1,19 @@
 import { useContext } from "react";
 
+import { PlayerContext } from "../store/player-context";
 import { QuizContext } from "../store/quiz-context";
 
 export default function Quiz() {
   const { quizItems } = useContext(QuizContext);
+  const { player } = useContext(PlayerContext);
 
   return (
     <>
-      {!quizItems && <p>There is no questions</p>}
+      <div>
+        <p>player name:{player.playerName}</p>
+        <p>current score: {player.currentScore}</p>
+        <p>highs score: {player.highScore}</p>
+      </div>
       {quizItems &&
         quizItems.map(item => <p key={item.question}>{item.question}</p>)}
     </>
