2f76dcc3c8f3a039ee325c8c9bb6a3c7d645845d
000begana000 <begana.contact@gmail.com>
2025-11-07 12:33:35 +0100

    fix: remove answers and answerState states

--- src/store/quiz-context.jsx
+++ src/store/quiz-context.jsx
@@ -3,10 +3,7 @@ import { createContext, useState } from "react";
 export const QuizContext = createContext({
   quizItems: [],
   playedCategories: [],
-  playerAnswers: [],
-  answerState: "unanswered",
   onSaveQuizItems: resData => {},
-  onSaveAnswer: newAnswer => {},
   onSavePlayedCategory: newCategory => {},
   onResetPlayedCategory: () => {},
 });
@@ -14,17 +11,10 @@ export const QuizContext = createContext({
 export default function QuizContextProvider({ children }) {
   const [quizItems, setQuizItems] = useState([]);
   const [playedCategories, setPlayedCategories] = useState([]);
-  const [playerAnswers, setPlayerAnswers] = useState([]);
-  const [answerState, setAnswerState] = useState("unanswered");
 
   function handleSaveQuizItems(resData) {
     setQuizItems(resData);
   }
-
-  function handleSaveAnswer(newAnswer) {
-    setPlayerAnswers(prevAnswers => [...prevAnswers, newAnswer]);
-  }
-
   function handleSavePlayedCategory(newCategory) {
     setPlayedCategories(prevCategories => [...prevCategories, newCategory]);
   }
@@ -36,10 +26,7 @@ export default function QuizContextProvider({ children }) {
   const ctxValue = {
     quizItems,
     playedCategories,
-    playerAnswers,
-    answerState,
     onSaveQuizItems: handleSaveQuizItems,
-    onSaveAnswer: handleSaveAnswer,
     onSavePlayedCategory: handleSavePlayedCategory,
     onResetPlayedCategory: handleResetPlayedCategories,
   };
