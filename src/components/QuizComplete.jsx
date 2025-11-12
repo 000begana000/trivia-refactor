d06bed1f636dd06c05c3f66ad7eb11e819b35fe3
000begana000 <begana.contact@gmail.com>
2025-11-12 12:50:57 +0100

    feat: display questions and answers, reset answers

---
+++ src/components/QuizComplete.jsx
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+
+import { QuizContext } from "../store/quiz-context";
+
+export default function QuizComplete({ onChangePage }) {
+  const { quizItems, answers, onResetAnswers } = useContext(QuizContext);
+
+  // Change page to categories
+  function handleChangePage() {
+    onChangePage("categories");
+    onResetAnswers();
+  }
+
+  return (
+    <>
+      <h1>Quiz Complete</h1>
+      {quizItems.map(item => (
+        <p>{item.question}</p>
+      ))}
+      {answers.map(answer => (
+        <p>{answer}</p>
+      ))}
+      <button onClick={handleChangePage}>Continue Play</button>
+    </>
+  );
+}
