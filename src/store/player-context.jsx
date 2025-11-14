df219153de936ce6d25ef3f19b4c66a4e051737b
000begana000 <begana.contact@gmail.com>
2025-11-14 18:38:55 +0100

    fix: remove currentScore

--- src/store/player-context.jsx
+++ src/store/player-context.jsx
@@ -1,30 +1,21 @@
 import { createContext, useState } from "react";
 
 export const PlayerContext = createContext({
-  player: { playerName: "", currentScore: 0, highScore: 0 },
+  player: { playerName: "", highScore: 0 },
   onCreatePlayer: () => {},
-  onIncreaseCurrentScore: () => {},
 });
 
 export default function PlayerContextProvider({ children }) {
   const [player, setPlayer] = useState("");
 
   function handleCreatePlayer(playerName) {
-    const newPlayer = { playerName, currentScore: 0, highScore: 0 };
+    const newPlayer = { playerName, highScore: 0 };
     setPlayer(newPlayer);
-  }
-
-  function handleIncreaseCurrentScore() {
-    setPlayer(prevState => ({
-      ...prevState,
-      currentScore: (prevState.currentScore += 100),
-    }));
   }
 
   const ctxValue = {
     player,
     onCreatePlayer: handleCreatePlayer,
-    onIncreaseCurrentScore: handleIncreaseCurrentScore,
   };
 
   return (
