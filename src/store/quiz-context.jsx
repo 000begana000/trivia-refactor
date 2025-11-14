05df67c53a8d33bfdf3e0b94308ca89d71711b6c
000begana000 <begana.contact@gmail.com>
2025-11-14 18:39:17 +0100

    fix: remove answers state

--- src/store/quiz-context.jsx
+++ src/store/quiz-context.jsx
@@ -3,18 +3,14 @@ import { createContext, useState } from "react";
 export const QuizContext = createContext({
   quizItems: [],
   playedCategories: [],
-  answers: [],
   onSaveQuizItems: resData => {},
   onSavePlayedCategory: newCategory => {},
-  onSaveAnswer: newAnswer => {},
   onResetPlayedCategory: () => {},
-  onResetAnswers: () => {},
 });
 
 export default function QuizContextProvider({ children }) {
   const [quizItems, setQuizItems] = useState([]);
   const [playedCategories, setPlayedCategories] = useState([]);
-  const [answers, setAnswers] = useState([]);
 
   function handleSaveQuizItems(resData) {
     setQuizItems(resData);
@@ -24,27 +20,16 @@ export default function QuizContextProvider({ children }) {
     setPlayedCategories(prevCategories => [...prevCategories, newCategory]);
   }
 
-  function handleSaveAnswer(newAnswer) {
-    setAnswers(prevAnswers => [...prevAnswers, newAnswer]);
-  }
-
   function handleResetPlayedCategories() {
     setPlayedCategories([]);
   }
 
-  function handleResetAnswers() {
-    setAnswers([]);
-  }
-
   const ctxValue = {
     quizItems,
-    answers,
     playedCategories,
     onSaveQuizItems: handleSaveQuizItems,
     onSavePlayedCategory: handleSavePlayedCategory,
-    onSaveAnswer: handleSaveAnswer,
     onResetPlayedCategory: handleResetPlayedCategories,
-    onResetAnswers: handleResetAnswers,
   };
 
   return (
