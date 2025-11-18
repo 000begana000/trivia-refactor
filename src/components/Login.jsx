58aba7ad06dce3658f9c3919d078769d0e4247fd
000begana000 <begana.contact@gmail.com>
2025-11-18 12:00:59 +0100

    refector: back to the state without validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,45 +1,24 @@
-import { useRef, useContext, useState } from "react";
+import { useRef, useContext } from "react";
 
 import { PlayerContext } from "../store/player-context";
 
 export default function Login({ onChangePage }) {
-  const [selectedPlayerName, setSeletedPlayerName] = useState("");
-  const [validPlayerName, setValidPlayerName] = useState(false);
-
-  const { players, onCreatePlayer, onSelectPlayer, onSavePlayer } =
-    useContext(PlayerContext);
+  const { onCreatePlayer } = useContext(PlayerContext);
 
   const playerName = useRef();
 
-  function handleSavePlayerName(event) {
+  function handleValidatePlayerName(event) {
     event.preventDefault();
 
-    const newPlayer = playerName.current.value;
-
-    const valid = players.every(player => player.playerName !== newPlayer);
-
-    if (valid) {
-      onCreatePlayer(playerName.current.value);
-      setSeletedPlayerName(playerName.current.value);
-      setValidPlayerName(true);
-      onSavePlayer();
-    }
-  }
-
-  function handleSelectPlayer(selectedPlayerName) {
-    onSelectPlayer(selectedPlayerName);
-    setSeletedPlayerName(selectedPlayerName);
-  }
-
-  function handleStartGame() {
+    onCreatePlayer(playerName.current.value);
     onChangePage("categories");
   }
 
   return (
     <div>
       <h1>Welcome to trivia quiz</h1>
-      <form action="" onSubmit={handleSavePlayerName}>
-        <p>Please enter your player name</p>
+      <form action="" onSubmit={handleValidatePlayerName}>
+        <h3>Please enter your player name</h3>
         <div className="input">
           <input
             type="text"
@@ -51,22 +30,7 @@ export default function Login({ onChangePage }) {
           />
           <button type="submit">save</button>
         </div>
-        {!validPlayerName && <p>the player name is already exist</p>}
-        {validPlayerName && <p>player name saved</p>}
       </form>
-      <div>
-        <h4>Select a player</h4>
-        {players.map(player => (
-          <button
-            key={player.playerName}
-            onClick={() => handleSelectPlayer(player.playerName)}
-          >
-            {player.playerName}
-          </button>
-        ))}
-      </div>
-      {selectedPlayerName && <p>Hello, {selectedPlayerName}!</p>}
-      <button onClick={handleStartGame}>Start Game</button>
     </div>
   );
 }
