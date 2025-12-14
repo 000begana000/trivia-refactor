b9c6a3220262eecf8e48a4c00ce07c8476d30da3
000begana000 <begana.contact@gmail.com>
2025-12-14 14:57:22 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
-import vercel from "vite-plugin-vercel";
+import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: process.env.PORT as unknown as number,
-  },
-  plugins: [vercel()],
+  plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/trivia-refactor",
 });
-
